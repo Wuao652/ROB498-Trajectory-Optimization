4b341ca4817dabe99ccfec98f8048d4b772c4333
wuaoliu <wuaoliu52@gmail.com>
2023-04-17 17:17:36 -0400

    add install pybullet and .gitignore

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Create a Conda environment
-conda create -n myenv 
-
+conda create -n myenv python=3.10
 conda activate myenv
 
-python3 demp.py
+# install pybullet
+git clone https://github.com/benelot/pybullet-gym.git
+cd pybullet-gym
+pip install -e .
+
+cd ..
+python3 demo.py
